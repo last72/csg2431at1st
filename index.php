fee286b5d3d08a16c072d3a97af014aa067ae6d9
TashiDuks <tashee2007@gmail.com>
2018-08-28 16:36:32 +0800

    Re-design of index page has completed but not linked with database. will be done soon.

--- index.php
+++ index.php
@@ -1,55 +1,83 @@
 <?php
-    require './func/dbconnection.php';
-    //Start or resume login session
-    session_start();
-    
-    //If the "uname" session variable is not set or is empty, redirect to login page
-    if ( !isset($_SESSION['uname']) || $_SESSION['uname'] == '' )
-  {
-    header('Location: login.php');
-    exit;
-  }
-    
-    $inactive = 900; // Set timeout period in seconds
-
-    if (isset($_SESSION['last_login_timestamp'])) {
-        $session_life = time() - $_SESSION['last_login_timestamp'];
-        if ($session_life > $inactive) {
-            session_destroy();
-            header("Location: logout.php");
-            echo "session timeout.";
-        }
-}
-$_SESSION['last_login_timestamp'] = time();
+    //php code goes here
+	include "./func/actions.php";
 ?>
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Login</title>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> 
-<style type="text/css">
-    .FormLayout{
-    	margin: 20px;
-    }
-</style>
-</head>
-<body>
-	<h1>Index page</h1>
-	<p>
-		<?php
-			//display the session here
-			echo 'Welcom, '.$_SESSION['uname'].'';
-			if((time() - $_SESSION['last_login_timestamp']) > 900) // 900 = 15 * 60 (15 Mins)
-			{
-				header("location:logout.php");
-			}
-		?>
-	</p>
-	<a href="logout.php">Logout</a></li>
-</body>
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <title>Hello Movies</title>
+  </head>
+  <body>
+    <!-- CONTAINER -->
+    <div class="container">
+    	<!-- NAVIGATION SECTION GOES HERE -->
+	  	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+		  <a class="navbar-brand" href="#">mOvIe</a>
+		  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		    <span class="navbar-toggler-icon"></span>
+		  </button>
+
+		  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+		    <ul class="navbar-nav mr-auto">
+		      <li class="nav-item active">
+		        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">Link</a>
+		      </li>
+		      <li class="nav-item dropdown">
+		        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+		          Dropdown
+		        </a>
+		        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+		          <a class="dropdown-item" href="#">Action</a>
+		          <a class="dropdown-item" href="#">Another action</a>
+		          <div class="dropdown-divider"></div>
+		          <a class="dropdown-item" href="#">Something else here</a>
+		        </div>
+		      </li>
+		      <li class="nav-item">
+		        <a class="nav-link" href="#">Sign In</a>
+		      </li>
+		       <li class="nav-item">
+		        <a class="nav-link" href="#">Register</a>
+		      </li>
+		    </ul>
+		    <form class="form-inline my-2 my-lg-0">
+		      <input class="form-control mr-sm-2" type="search" placeholder="Search movies" aria-label="Search">
+		      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+		    </form>
+		  </div>
+		</nav>
+	  	<!-- END OF NAVIATION -->
+    	<h1>DISPLAY ALL MOVIES HERE!!!</h1>
+    	<table class="table table-bordered">
+    		<tr>
+    			<th>Movie ID</th>
+    			<th>Title</th>
+    			<th>Released On</th>
+    			<th>Director</th>
+    			<th>&nbsp;</th>
+    			<th>&nbsp;</th>
+    		</tr>
+    		<?php
+    			//record fetch call
+    		?>
+    	</table>
+    </div>
+    <!-- END OF CONTAINER -->
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
 </html>
