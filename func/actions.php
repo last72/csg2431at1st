85fdbc8dd52312c75db9a041d50c99327c751d2f
TashiDuks <tashee2007@gmail.com>
2018-09-07 18:34:35 +0800

    All data manipulation function goes here.
    I have added few functions for data manipulation. We will use this functions in entire project and will be added as when we go futher.

---
+++ func/actions.php
@@ -0,0 +1,41 @@
+<?php
+	include "dbconnection.php";
+
+	class DataOperation extends Database
+	{
+		/* Functions to display all records */
+		public function fetch_record($table){
+			$sql = "SELECT * FROM ".$table;
+			$array = array();
+			$query = mysqli_query($this->strConn,$sql);
+			while($row = mysqli_fetch_assoc($query)){
+				$array[] = $row;
+			}
+			return $array;
+		}
+		/* Insert Record Function */
+		public function insert_record(){
+
+		}
+		/* Delete Record Function */
+		public function delete_record(){
+
+		} 
+		/* Delete Record Function */
+		public function update_record(){
+			
+		}
+		/* Select record with JOIN statement */
+		public function join_fetch_record($table){
+			$sql="SELECT movies.movie_id, movies.movie_name, movies.release_year, movies.director, sum(ratings.rating) FROM ".$table." INNER JOIN ratings ON movies.movie_id = ratings.movie_id GROUP BY movies.movie_id";
+			$array = array();
+			$query = mysqli_query($this->strConn,$sql);
+			while($row = mysqli_fetch_assoc($query)){
+				$array[] = $row;
+			}
+			return $array;
+		}
+	}
+	$obj = new DataOperation;
+
+?>
