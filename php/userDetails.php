80d9f69f28894522466049c8c4ef4d77d3b7666f
Woongyeol Choi <cwy5847@gmail.com>
2018-09-13 12:01:02 +0800

    db connection in user details

--- php/userDetails.php
+++ php/userDetails.php
@@ -32,7 +32,7 @@ session_start();
   $query = "SELECT * FROM users ".'WHERE username = \''.$_GET['username'].'\'';
   
   // execute the query
-  $results = $db->query($query);
+  $results = mysqli_query($connection, $query);
   
   // show how many rows the query returned
   
